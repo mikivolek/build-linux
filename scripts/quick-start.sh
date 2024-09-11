a25ffbb7f2cc6e822a335264b79f201f482f70b5
Miroslav Volek <78548302+mikivolek@users.noreply.github.com>
2024-09-11 16:15:42 +0200

    Update quick-start.sh

--- scripts/quick-start.sh
+++ scripts/quick-start.sh
@@ -15,7 +15,7 @@ print_blue()
 
 function usage()
 {
-    print_blue "USAGE: $0 [[asus-tp370ql|hp-envy-x2]lenovo-miix-630|lenovo-yoga-c630|lenovo-thinkpad-t14s|generic]|kernel|grub|help][-f]"
+    print_blue "USAGE: $0 [[lenovo-thinkpad-t14s|generic]|kernel|grub|help][-f]"
     return 1
 }
 
@@ -25,7 +25,7 @@ fi
 
 while [ $# -gt 0 ]; do
     case $1 in
-        asus-tp370ql|hp-envy-x2|lenovo-miix-630|lenovo-yoga-c630|lenovo-thinkpad-t14s|generic)
+        lenovo-thinkpad-t14s|generic)
             DEVICE=$1
             ;;
         kernel)
